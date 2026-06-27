ea6ff1f0ebcd765a51ea9078df9a2cc5d70187af
dj120599 <60100701+dj120599@users.noreply.github.com>
2026-06-27 17:55:20 +0100

    Revert "criação class aula e o que corresponde"

--- GinasioVitaFit.WebClient/GinasioVitaFit.WebClient/Program.cs
+++ GinasioVitaFit.WebClient/GinasioVitaFit.WebClient/Program.cs
@@ -1,13 +1,10 @@
 using GinasioVitaFit.WebClient.Components;
-using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-builder.Services.AddMudServices();
 
 var app = builder.Build();
 
