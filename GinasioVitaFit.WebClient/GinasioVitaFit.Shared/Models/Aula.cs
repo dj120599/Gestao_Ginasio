e76282ad14875cf59223c7bf550bf285be5f891d
LopesR1993 <ruben.lopes93@gmail.com>
2026-06-30 18:23:37 +0100

    Fix project references

--- GinasioVitaFit.WebClient/GinasioVitaFit.Shared/Models/Aula.cs
+++ GinasioVitaFit.WebClient/GinasioVitaFit.Shared/Models/Aula.cs
@@ -1,18 +1,12 @@
-<<<<<<< HEAD:GinasioVitaFit.WebClient/GinasioVitaFit.WebClient/Models/Aula.cs
-namespace GinasioVitaFit.WebClient.Models;
-=======
-using System.ComponentModel.DataAnnotations;
-
 namespace GinasioVitaFit.Shared.Models;
->>>>>>> 83a36622a588474e7e7ffb93320cf9562ce2369d:GinasioVitaFit.WebClient/GinasioVitaFit.Shared/Models/Aula.cs
 
 public class Aula
 {
-    
+
     public int AulaID { get; set; }
-    
+
     public string Instrutor { get; set; }
-    
+
     public string Modalidade { get; set; }
 
     public string Sala { get; set; }
@@ -22,6 +16,6 @@ public class Aula
     public DateTime Inicio { get; set; }
 
     public DateTime Fim { get; set; }
-    
+
     public bool Delete { get; set; } = false;
 }
