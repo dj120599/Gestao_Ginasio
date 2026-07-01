0852b03a075cba825e8fd9fe8a4dec70a3c7c7a6
KAWANNY <kawannyaraujo629@gmail.com>
2026-07-01 20:01:59 +0100

    default para image pronto

--- GinasioVitaFit.WebClient/GinasioVitaFit.Api/Entities/Socio.cs
+++ GinasioVitaFit.WebClient/GinasioVitaFit.Api/Entities/Socio.cs
@@ -5,6 +5,7 @@ public class Socio : BaseEntity
     public string Name { get; set; }
     public DateTime Nascimento { get; set; }
     public string Email { get; set; }
+    public string ImageUrl { get; set; }
     public string Contacto { get; set; }
     public Plano Plano { get; set; }
     public DateTime SubInicio { get; set; }
