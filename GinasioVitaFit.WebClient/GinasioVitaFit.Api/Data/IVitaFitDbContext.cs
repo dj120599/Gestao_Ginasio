739bed549aa4c1a7a628168a7ed56cc866d61138
dj120599 <davidfvjorge@gmail.com>
2026-07-01 18:43:40 +0100

    Adicionar classes AulaSocio e InstrutorMod

--- GinasioVitaFit.WebClient/GinasioVitaFit.Api/Data/IVitaFitDbContext.cs
+++ GinasioVitaFit.WebClient/GinasioVitaFit.Api/Data/IVitaFitDbContext.cs
@@ -11,6 +11,8 @@ public interface IVitaFitDbContext
     public DbSet<Modalidade> Modalidades { get; set; }
     public DbSet<Plano> Planos { get; set; }
     public DbSet<Socio> Socios { get; set; }
+    public DbSet<InstrutorMod> InstrutorMods { get; set; }
+    public DbSet<AulaSocios> AulaSocios { get; set; }
     
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
---
+++ GinasioVitaFit.WebClient/GinasioVitaFit.Api/Models/InstrutorModDto.cs
@@ -0,0 +1,9 @@
+namespace GinasioVitaFit.Api.Models;
+
+public class InstrutorModDto
+{
+    public int Id { get; set; }
+    public int InstrutorID {get;set;}
+    public int ModalidadeID {get;set;}
+    public bool IsDeleted { get; set; }
+}
