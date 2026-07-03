e84397b1cfcfde386a1059b21a239fb174dc5e76
dj120599 <davidfvjorge@gmail.com>
2026-07-03 18:59:24 +0100

    Remake Aulas

--- GinasioVitaFit.WebClient/GinasioVitaFit.Api/Entities/Aula.cs
+++ GinasioVitaFit.WebClient/GinasioVitaFit.Api/Entities/Aula.cs
@@ -4,7 +4,7 @@ public class Aula : BaseEntity
 {
     public Instrutor Instrutor { get; set; }
     public Modalidade Modalidade { get; set; }
-    public string Sala { get; set; }
+    public Sala Sala { get; set; }
     public int Capacidade { get; set; }
     public DateTime Inicio { get; set; }
     public DateTime Fim { get; set; }
--- GinasioVitaFit.WebClient/GinasioVitaFit.Api/Models/AulaDto.cs
+++ GinasioVitaFit.WebClient/GinasioVitaFit.Api/Models/AulaDto.cs
@@ -5,7 +5,7 @@ public class AulaDto
     public int Id { get; set; }
     public InstrutorDto Instrutores { get; set; }
     public ModalidadeDto Modalidades { get; set; }
-    public string Sala { get; set; }
+    public SalaDto Sala { get; set; }
     public int Capacidade { get; set; }
     public DateTime Inicio { get; set; }
     public DateTime Fim { get; set; }
