72bd538389e945ddfb547f725810183b1f2038d3
dj120599 <davidfvjorge@gmail.com>
2026-07-07 16:39:14 +0100

    Adding the function add socio to Aula e função desisitir de aula.

--- GinasioVitaFit.WebClient/GinasioVitaFit.Shared/Models/Aula.cs
+++ GinasioVitaFit.WebClient/GinasioVitaFit.Shared/Models/Aula.cs
@@ -3,7 +3,7 @@ namespace GinasioVitaFit.Shared.Models;
 public class Aula
 {
 
-    public int AulaID { get; set; }
+    public int Id { get; set; }
 
     public Instrutor Instrutor { get; set; }
 
@@ -13,12 +13,9 @@ public class Aula
 
     public int Capacidade { get; set; }
 
-    public DateTime Inicio { get; set; }
+    public DateTime AulaInicio { get; set; }
 
-    public DateTime Fim { get; set; }
+    public DateTime AulaFim { get; set; }
 
-    public bool Delete { get; set; } = false;
-    
-    public string ImagemUrl { get; set; } = "https://plus.unsplash.com/premium_photo-1746421978363-6e3ea7668a73?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-    
+    public bool IsDeleted { get; set; } = false;
 }
